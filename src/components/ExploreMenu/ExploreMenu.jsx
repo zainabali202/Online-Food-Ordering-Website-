5619a271294aa1cac26decc15a94d70ffbec42bc
Zainab Ali <159950917+zainabali202@users.noreply.github.com>
2026-05-18 18:26:35 +0530

    Add files via upload

---
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,43 @@
+import React, { useContext } from 'react'
+import './ExploreMenu.css'
+import { StoreContext } from '../../Context/StoreContext'
+
+const ExploreMenu = ({category,setCategory}) => {
+
+  const {menu_list} = useContext(StoreContext);
+  
+  return (
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore Our Menu</h1>
+      <p className="explore-menu-text">
+        Explore a wide variety of delicious dishes crafted to delight your taste
+        buds. We’re here to make every meal memorable and satisfying. 
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name,
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                src={item.menu_image}
+                className={category === item.menu_name ? "active" : ""}
+                alt=""
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
+    </div>
+  );
+}
+
+export default ExploreMenu
